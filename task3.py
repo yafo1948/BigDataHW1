ec81e983cc293ba00e0cb61541f9f17baed4de6a
jofo2021 <yardenfogel1948@gmail.com>
2021-11-04 19:19:36 +0200

    task 3.1-3.3

--- task3.py
+++ task3.py
@@ -8,9 +8,10 @@ import sqlite3
 
 import numpy as np
 import pandas as pd
-import pyarrow.csv as pv
-import pyarrow.parquet as pq
+#import pyarrow.csv as pv
+#import pyarrow.parquet as pq
 import dask.dataframe as dd
+import os
 
 PANDAS_FILE = 'mydatapandas.parquet'
 
@@ -24,32 +25,25 @@ NUM_OF_RECORDS = 10
 
 
 def main():
-    file = open(MYDATA_CSV)
-    reader = csv.reader(file)
-    lines = len(list(reader))
-    print(lines)
+    b = os.path.getsize(MYDATA_CSV)
+    print(b)
+    middle = int(b/2)
+    print(middle)
+    first_chunk(middle)
+    last_chunk(middle)
 
-    # Convert csv to Parquet
+def first_chunk(middle):
+    file = open(MYDATA_CSV,"rb")
+    arr=list(file.read(middle).decode(encoding='utf-8'))
+    print(arr)
+    print(arr.count('\n')+1)
 
-    # Using PyArrow
-    table = pv.read_csv(MYDATA_CSV)
-    pq.write_table(table, '%s' % PYARROW_FILE)
-
-    # Using Dask
-    df = dd.read_csv(MYDATA_CSV)
-    df.to_parquet('%s' % DASK_FILE, write_index=False)
-
-    # Using Panadas
-    df = pd.read_csv(MYDATA_CSV)
-    df.to_parquet('%s' % PANDAS_FILE)
-
-
-    # The diffences between Dask and the other two methods of converting the
-    # CSV file into Parquet are
-    # 1. Dask also contains a metadata folder
-    # 2. Dask is not a pure library but more of a parallel computation framework
-    # (much like spark)
-    # 3. <Jordan - fill in the rest please>
+def last_chunk(middle):
+    file = open(MYDATA_CSV, "rb")
+    file.seek(middle,0)
+    arr1 = list(file.read(middle).decode(encoding='utf-8'))
+    print(arr1)
+    print(arr1.count('\n'))
 
 if __name__ == '__main__':
     main()
